9abedca724fa9f849fe3f2ef57d3f89b58a305f2
sanea <sasagherciu22@gmail.com>
2026-09-03 15:56:35 +0300

    Initialize D&D game project structure

---
+++ HealthMonitor DB.session.sql
@@ -0,0 +1,19 @@
+{
+  "Id": 18,
+  "Name": "admin",
+  "Email": "admin@omnitrack.com",
+  "Password": "8358a77c17fac3c6a9c67e6dea9d1fda1e74e2a67fe138789317449f23176ca3",
+  "Gender": "male",
+  "Age": 19,
+  "Height": 153,
+  "Weight": 30,
+  "Goal": "lose",
+  "RegisteredOn": "2026-06-10 21:00:00.815387+03",
+  "Role": 1,
+  "OnboardingCompleted": true,
+  "ResetPasswordCode": null,
+  "TwoFactorCode": null,
+  "TwoFactorEnabled": false,
+  "Bio": null,
+  "PasswordSalt": "E4x3jt+t9dW6oRttwIVb0w=="
+}
