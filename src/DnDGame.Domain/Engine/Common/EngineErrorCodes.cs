d16d274ae36bf6c9b8611de47c93f52ed5971023
Danil1325 <danil.strelciuc@gmail.com>
2026-09-10 11:39:49 +0300

    Upate nr1 game engine

---
+++ src/DnDGame.Domain/Engine/Common/EngineErrorCodes.cs
@@ -0,0 +1,14 @@
+namespace DnDGame.Domain.Engine.Common;
+
+/// <summary>
+/// Stable error codes returned by game-engine operations.
+/// </summary>
+public static class EngineErrorCodes
+{
+    public const string BattleNotFound = "BATTLE_NOT_FOUND";
+    public const string BattleAlreadyFinished = "BATTLE_ALREADY_FINISHED";
+    public const string NotPlayerTurn = "NOT_PLAYER_TURN";
+    public const string InvalidAction = "INVALID_ACTION";
+    public const string PlayerDead = "PLAYER_DEAD";
+    public const string EnemyDead = "ENEMY_DEAD";
+}
